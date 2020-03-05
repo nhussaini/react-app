9374de797444c5d9f5cfb5310f8dab6edfaac981
unknown <nhussaini@luc.edu>
2020-03-05 07:52:18 -0800

    added dynamic content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const person =()=>{
-    return <h1>I am a person</h1>
+    return <h1>I am a person and {Math.floor(Math.random()*30)} years old</h1>
 }
 
 export default person
