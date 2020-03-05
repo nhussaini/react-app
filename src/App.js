c67a948bec3a7686f0963b7f16377c0f9ef3102a
unknown <nhussaini@luc.edu>
2020-03-05 08:03:11 -0800

    added the first props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     return (
       <div className="App">
        <h1>I am a react app</h1>
-       <Person />
+       <Person name="Max" age="25"/>
+       <Person name="nasr" age="60"/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
-const person =()=>{
-    return <h1>I am a person and {Math.floor(Math.random()*30)} years old</h1>
+const person =(props)=>{
+    return <h1>I am {props.name} and {props.age} years old</h1>
 }
 
 export default person
