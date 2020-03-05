03b35cbec8780bae3fde25d31a0b2579e80e6c10
unknown <nhussaini@luc.edu>
2020-03-05 13:20:37 -0800

    added useState hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    persons: [
-      {name: 'Max', age:28},
-      {name: 'Manu', age:21},
-      {name: 'Stephanie', age:88}
-    ]
-  }
-  render() {
+const app= props => {
+  const [ personsState, setPersonsState]=useState(//this is a new feature in javascript, called array destructring
+     {
+      persons: [
+        {name: 'Max', age:28},
+        {name: 'Manu', age:21},
+        {name: 'Stephanie', age:88}
+      ],
+      otherState: 'some other value'  
+    }); 
+
+    const switchNameHandler= () =>{
+      //console.log('was clicked!');
+      setPersonsState({
+        persons: [
+        {name: 'Maxilium', age:28},
+        {name: 'Manu', age:21},
+        {name: 'Stephanie', age:5}
+        ]
+      });
+    };
+  
+
+  
     return (
       <div className="App">
        <h1>I am a react app</h1>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> my hobby:soccer</Person>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+       <button onClick={switchNameHandler}>Switch Name!</button>
+       <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> my hobby:soccer</Person>
+       <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
-  }
+   
 }
 
-export default App;
+export default app;
+
+
+
+
