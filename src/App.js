adc5a4537b79298f4354dea8a2e53a09c7ca3ead
unknown <nhussaini@luc.edu>
2020-03-08 10:24:11 -0700

    outputing lists

--- src/App.js
+++ src/App.js
@@ -44,9 +44,12 @@ class App extends Component {
     if (this.state.showPersons){
       persons=(
         <div>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler}> my hobby:soccer</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          {this.state.persons.map(person => {
+          return <Person
+            name={person.name}
+            age={person.age}
+          />
+        })}
      </div>
       );
     }
