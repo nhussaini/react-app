335cac5208af5964ea3fc0a02741cee41c65fe1f
unknown <nhussaini@luc.edu>
2020-03-08 15:20:25 -0700

    flexible lists

--- src/App.js
+++ src/App.js
@@ -5,33 +5,35 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      {name: 'Max', age:28},
-      {name: 'Manu', age:21},
-      {name: 'Stephanie', age:88}
+      {id:'fda',name: 'Max', age:28},
+      {id:'ghdfg',name: 'Manu', age:21},
+      {id:'gfsdv',name: 'Stephanie', age:88}
     ],
     showPersons:false
   }
-  switchNameHandler= () =>{
-    //console.log('was clicked!');
+  
+
+  nameChangedHandler = (event,id) =>{
+    const personIndex=this.state.persons.findIndex(p =>{
+      return p.id===id;
+    });
+      const person= {... this.state.persons[personIndex]};
+      person.name=event.target.value;
+
+      const persons= [...this.state.persons];
+      persons[personIndex] = person;
+
+    
     this.setState({
-      persons: [
-      {name: 'Maxilium', age:28},
-      {name: 'Manu', age:21},
-      {name: 'Stephanie', age:88}
-      ],
-      
+      persons:persons
     })
   }
 
-  nameChangedHandler = (event) =>{
-    this.setState({
-      persons: [
-      {name: 'Maxilium', age:28},
-      {name: event.target.value, age:21},
-      {name: 'Stephanie', age:88}
-      ],
-      
-    })
+  deletePersonHandler = (personIndex) =>{
+    //const persons = this.state.persons;
+    const persons = [...this.state.persons];
+    persons.splice(personIndex,1);
+    this.setState({persons:persons}); 
   }
 
   togglePersonsHandler = () => {
@@ -44,10 +46,13 @@ class App extends Component {
     if (this.state.showPersons){
       persons=(
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person,index) => {
           return <Person
+          click={()=>this.deletePersonHandler(index)}
             name={person.name}
             age={person.age}
+            key={person.id}
+            changed={(event)=>this.nameChangedHandler(event,person.id)}
           />
         })}
      </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
 const person =(props)=>{
     return (
-    <div>
-    <h1>I am {props.name} and {props.age} years old</h1>
+    <div className="Person">
+<p onClick={props.click}> I'm {props.name} and I am {props.age} years old</p>
     <h1>{props.children}</h1>
     <input type="text" onChange={props.changed} value={props.name}/>
     </div>
