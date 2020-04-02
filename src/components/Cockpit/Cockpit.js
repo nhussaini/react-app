19f29f4c7f0135fd2b06ec5a209bb7dffe3a09f8
unknown <nhussaini@luc.edu>
2020-04-02 09:25:45 -0700

    split the app into components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,28 @@
 import React from 'react';
-const cockpit=()=>{
-    return (
-        
+
+
+import classes from './Cockpit.css';
+
+const cockpit = (props) =>{
+    const assignedClasses = [];
+    let btnClass='';
+    if(props.showPersons){
+      btnClass=classes.red;
+    }
+    
+    if(props.persons.length<=2){
+      assignedClasses.push(classes.red);
+    }
+    if(this.state.persons.length<=1){
+      assignedClasses.push(classes.bold);
+    }
+    return(
+        <div className={classes.Cockpit}>
+        <h1>I am a react app</h1>
+        <p class={assignedClasses.join(' ')}>this is really working!</p>
+        <button className={btnClass.join(' ')} onClick={props.togglePersonsHandler}>Toggle Persons!</button>
+        </div>
     );
-};
+}
 
 export default cockpit;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import classes from  './App.css';
+
 import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+
 
 
 
@@ -45,34 +47,24 @@ class App extends Component {
   render() {
 
     let persons=null;
-    let btnClass = [classes.Button];
+  
 
     if (this.state.showPersons){
-      persons=(
-        <div>
-          <Persons
+      persons=<Persons
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
-          changed={this.nameChangedHandler} />
-          
-     </div>
-      );
-        btnClass.push(classes.Red);
+          changed={this.nameChangedHandler} />;
     }
-    let assignedClasses = [];
-    if(this.state.persons.length<=2){
-      assignedClasses.push(classes.red);
-    }
-    if(this.state.persons.length<=1){
-      assignedClasses.push(classes.bold);
-    }
+  
     return (
     
       <div className="classes.App">
-        <h1>I am a react app</h1>
-        <p class={assignedClasses.join(' ')}>this is really working!</p>
-        <button className={btnClass.join(' ')} onClick={this.togglePersonsHandler}>Toggle Persons!</button>
+       <Cockpit
+        showPersons={this.state.showPersons}
+        persons={this.state.persons}
+        clicked={this.togglePersonsHandler}/> 
        {persons} 
+
        
         
       
